45feef8667e8afa298e4b1eb6cefeed4bb31b643
Andrew Roberts <sombreroman55@gmail.com>
2020-10-24 13:30:24 -0600

    Adding more stuff

---
+++ palettes.h
@@ -0,0 +1,20 @@
+/* palettes.c
+ *
+ * Different, predefined palettes through the
+ * filling of paletter arrays with hex color codes 
+ */
+#include <stdint.h>
+
+uint32_t pastelle_rainbow[17] = {16, 0x051E3E, 0x251E3E, 0x451E3E, 0x651E3E, 
+                                 0x851E3E, 0xFF3377, 0xFF5588, 0xFFBBEE,
+                                 0xFF99CC, 0xFF77AA, 0xFF8B94, 0xFFAAA5,
+                                 0xFFD3B6, 0xDCEDC1, 0xA8E6CF, 0x88D8B0};
+
+uint32_t blue_greens[17] = {16, 0x011F4B, 0x011F4B, 0x011F4B, 0x03396C, 
+                            0x03396C, 0x005B96, 0x6497B1, 0xB3CDE0,
+                            0xB2D8D8, 0x66B2B2, 0x008080, 0x006666,
+                            0x006666, 0x004C4C, 0x004C4C, 0x004C4C};
+
+uint32_t magicant[10] = {9, 0x9EDDFF, 0xA0CBFF, 0xA0C4FF, 
+                        0xC8B6FF, 0xD8BBFF, 0xE7BFFF, 
+                        0xFFC6FF, 0xFFCEFF, 0xFFD9FF};
---
+++ renderer.h
@@ -0,0 +1,11 @@
+/* renderer.h
+ *
+ * Rendering function declarations
+ */
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
+#include <stdint.h>
+
+void render(uint32_t* pixels);
+
+#endif /* _RENDERER_H_ */
