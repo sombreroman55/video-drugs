657368a8ebbca5123edf4ad0d4250d9d3289a5b3
Andrew Roberts <sombreroman55@gmail.com>
2025-02-01 00:48:47 -0700

    Cleaning up this branch

--- renderer.c
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-#define LINE_TO_DEG 360 / 256
-
-static const double PI = atan(1) / 4;
-
-static int offset_from_angle (double degrees, double amplitude, double period, double shift);
-static int c_offset_from_angle (double degrees, double amplitude, double period, double shift);
